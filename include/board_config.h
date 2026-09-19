fcff47857627366633540ae94b610753de00e636
Piciu811 <aep82@o2.pl>
2026-09-19 20:56:29 +0200

    Add include/board_config.h

---
+++ include/board_config.h
@@ -0,0 +1,18 @@
+#pragma once
+#define TFT_QSPI_CS   12
+#define TFT_QSPI_SCK  17
+#define TFT_QSPI_D0   13
+#define TFT_QSPI_D1   18
+#define TFT_QSPI_D2   21
+#define TFT_QSPI_D3   14
+#define TFT_QSPI_RST  16
+#define TFT_BL         1
+#define TOUCH_IICSCL  10
+#define TOUCH_IICSDA  15
+#define TOUCH_INT     11
+#define TOUCH_RES     16
+#define PIN_BUTTON_1   0
+#define LCD_NATIVE_W 180
+#define LCD_NATIVE_H 640
+#define LCD_W        640
+#define LCD_H        180
