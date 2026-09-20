a50a29448fa5ff2b7198fc6aa68329cb7f13b9bf
Piciu811 <aep82@o2.pl>
2026-09-20 21:01:54 +0200

    Remove incomplete track database placeholder

--- src/tracks_europe.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <stdint.h>
-struct FactoryTrack { int32_t lat1,lon1,lat2,lon2; uint16_t id; uint8_t enabled; };
-static const FactoryTrack FACTORY_TRACKS[] PROGMEM = {
-};
-static const uint16_t FACTORY_TRACK_COUNT=0;
