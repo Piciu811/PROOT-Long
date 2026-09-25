0515f9ef6bf18876715765ddf57b81701589b6f6
Piciu811 <aep82@o2.pl>
2026-09-25 21:51:03 +0200

    Expand LapTimer module state and API

--- src/laptimer.h
+++ src/laptimer.h
@@ -4,18 +4,15 @@
 #include "racebox.h"
 
 struct LapTimerState {
-  uint32_t lapStartTow = 0;
-  uint32_t lastLapMs = 0;
-  uint32_t bestLapMs = 0;
-  int32_t deltaMs = 0;
-  bool deltaValid = false;
-  uint16_t lapCount = 0;
-  bool running = false;
-  bool stopped = false;
-  bool customLine = false;
-  bool factoryTrack = false;
-  int16_t factoryTrackIndex = -1;
-  uint32_t flashStarted = 0;
+  uint32_t lapStartTow=0,lastLapMs=0,bestLapMs=0;
+  int32_t deltaMs=0;
+  bool deltaValid=false;
+  uint16_t lapCount=0;
+  bool running=false,stopped=false;
+  bool customLine=false,factoryTrack=false;
+  int16_t factoryTrackIndex=-1;
+  uint32_t flashStarted=0;
+  uint8_t historyCount=0,historyPage=0;
 };
 
 namespace LapTimer {
@@ -23,5 +20,9 @@ namespace LapTimer {
   void update(const RaceBoxTelemetry &gps);
   void setCustomStartFinish(const RaceBoxTelemetry &gps);
   void stop();
+  void resume();
+  void historyPageUp();
+  void historyPageDown();
+  uint32_t history(uint8_t index);
   const LapTimerState &state();
 }
